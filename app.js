cf6ffb7a1bcfcf0ce03c17c5e0ebc7dc5b1282da
gcnaveen <naveengc99@gmail.com>
2025-08-14 15:09:19 +0530

    media

--- app.js
+++ app.js
@@ -6,8 +6,49 @@ const authRoutes = require('./routes/authRoutes');
 const staffRequestRoutes = require('./routes/staffRequestRoutes');
 const mediaRoutes = require('./routes/media');
 const app = express();
+// Schedule cron job to empty certain directories daily at 01:01 AM
+cron.schedule('01 01 * * *', () => {
+    const empty_these_directories = [
+        "assets/district_issues",
+        "assets/department_issues",
+        "assets/district_reports",
+        "assets/dis_reports",
+    ];
 
+    empty_these_directories.forEach((directory) => {
+        fs.readdir(directory, (err, files) => {
+            if (err) throw err;
+
+            for (const file of files) {
+                fs.unlink(path.join(directory, file), (err) => {
+                    if (err) throw err;
+                });
+            }
+        });
+    });
+});
+
+// Enable CORS
 app.use(cors());
+
+// Security headers with Helmet
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Logger middleware
+app.use(morgan("dev"));
+
+// Set payload limit to 2GB
+const payloadLimit = 2 * 1024 * 1024 * 1024; // 2GB in bytes
+app.use(express.json({ limit: payloadLimit }));
+app.use(express.urlencoded({ limit: payloadLimit, extended: true }));
+
+// Serve static assets folder
+const API_ROOT = '/';
+app.use(`${API_ROOT}assets`, express.static(path.join(__dirname, "assets")));
+
+// Disable etag for fresh content on each request
+app.disable('etag');
+
 app.use(express.json());
 
 // Routes
