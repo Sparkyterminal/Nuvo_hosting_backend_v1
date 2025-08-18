a1b729ce52aa26889e0592e1b769d2ba9b0b4010
sparkyterminal <shashankau17@gmail.com>
2025-08-18 22:42:40 +0530

    assets path

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const path = require("path");
 const cors = require("cors");
-const cron = require('node-cron');
-const fs = require("fs");const userRoutes = require('./routes/userRoutes');
-const eventRequestRoutes = require('./routes/eventRequestRoutes');
-const authRoutes = require('./routes/authRoutes');
-const staffRequestRoutes = require('./routes/staffRequestRoutes');
-const mediaRoutes = require('./routes/media');
+const cron = require("node-cron");
+const fs = require("fs");
+const userRoutes = require("./routes/userRoutes");
+const eventRequestRoutes = require("./routes/eventRequestRoutes");
+const authRoutes = require("./routes/authRoutes");
+const staffRequestRoutes = require("./routes/staffRequestRoutes");
+const mediaRoutes = require("./routes/media");
 const app = express();
 // Schedule cron job to empty certain directories daily at 01:01 AM
-cron.schedule('01 01 * * *', () => {
-    const empty_these_directories = [
-        "assets/temp_resources",
-        "assets/department_issues",
-        "assets/district_reports",
-        "assets/dis_reports",
-    ];
+cron.schedule("01 01 * * *", () => {
+  const empty_these_directories = [
+    "assets/temp_resources",
+    "assets/images",
+    "assets/district_reports",
+    "assets/dis_reports",
+  ];
 
-    empty_these_directories.forEach((directory) => {
-        fs.readdir(directory, (err, files) => {
-            if (err) throw err;
+  empty_these_directories.forEach((directory) => {
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
 
-            for (const file of files) {
-                fs.unlink(path.join(directory, file), (err) => {
-                    if (err) throw err;
-                });
-            }
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), (err) => {
+          if (err) throw err;
         });
+      }
     });
+  });
 });
 
 // Enable CORS
@@ -48,20 +49,19 @@ app.use(express.json({ limit: payloadLimit }));
 app.use(express.urlencoded({ limit: payloadLimit, extended: true }));
 
 // Serve static assets folder
-const API_ROOT = '/';
+const API_ROOT = "/";
 app.use(`${API_ROOT}assets`, express.static(path.join(__dirname, "assets")));
 
 // Disable etag for fresh content on each request
-app.disable('etag');
+app.disable("etag");
 
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/event-requests', eventRequestRoutes);
-app.use('/api/staff-requests', staffRequestRoutes);
-app.use('/api/media', mediaRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/event-requests", eventRequestRoutes);
+app.use("/api/staff-requests", staffRequestRoutes);
+app.use("/api/media", mediaRoutes);
 
 module.exports = app;
